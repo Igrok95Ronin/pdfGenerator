f5a6c15e3798a5bf67a457c20732bf24137df35b
Rizvan <xl92@mail.ru>
2023-07-07 17:10:27 +0300

    Добавили шрифты

--- cmd/pdfgenerator/app.go
+++ cmd/pdfgenerator/app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jung-kurt/gofpdf"
+	"os"
 )
 
 func main() {
@@ -9,26 +11,40 @@ func main() {
 	pdf.AddPage()                          //
 	pdf.SetFont("Arial", "B", 16)          // Задаем шрифт, жирность и размер
 
+	// Добавляем шрифт, поддерживающий больше символов
+	pdf.AddUTF8Font("Arial", "", "arial.ttf")
+
+	getwd, _ := os.Getwd()
+
+	fmt.Println(getwd)
+
 	//Заголовок документа
 	_, lineHt := pdf.GetFontSize() // высота строки зависит от размера шрифта
 	pdf.CellFormat(190, lineHt, "F A K T U R A", "0", 0, "C", false, 0, "")
 
-	//Левый верхний блок
+	//Верхний блок
+	//Первая строка
 	pdf.SetFont("Arial", "", 12)
 	pdf.Ln(lineHt * 2) //переход на новую строку
 	pdf.CellFormat(95, lineHt, "Kamil Teplý", "0", 0, "L", false, 0, "")
 
+	//Правый блок ID
 	pdf.SetFont("Arial", "B", 12)
 	pdf.SetTextColor(52, 116, 178) //задает цвет текста rgb
 	pdf.CellFormat(95, lineHt, "ID: CZ-3135", "0", 0, "R", false, 0, "")
 
+	//Вторая строка
 	pdf.SetFont("Arial", "", 12)
 	pdf.SetTextColor(0, 0, 0)
 	pdf.Ln(lineHt)
 	pdf.CellFormat(190, lineHt, "Francouzská 2", "0", 0, "L", false, 0, "")
 
-	//
-	err := pdf.OutputFileAndClose("yourContract.pdf")
+	//Третья строка
+	pdf.Ln(lineHt)
+	pdf.CellFormat(190, lineHt, "12000 Praha", "0", 0, "L", false, 0, "")
+
+	//Создаем pdf файл
+	err := pdf.OutputFileAndClose("../../yourContract.pdf")
 	if err != nil {
 		panic(err)
 	}
